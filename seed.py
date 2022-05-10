7e78371bece6cfda9838fb9f330f29c04c1a222f
pt3th <pt3th@163.com>
2022-05-10 09:43:57 +0200

    fixed random seed

---
+++ seed.py
@@ -0,0 +1,7 @@
+def seed(seed=0):
+    random.seed(seed)  # python random generator
+    np.random.seed(seed)  # numpy random generator
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
